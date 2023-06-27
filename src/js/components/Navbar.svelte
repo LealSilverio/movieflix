9b3ca1c1a4665459f773bb70191cbb84c4892ff8
Sara Silverio <saralealsilverio@gmail.com>
2023-06-27 15:09:22 -0600

    nav bar routing

---
+++ src/js/components/Navbar.svelte
@@ -0,0 +1,15 @@
+
+<script>
+    import { userStore } from "../../lib/stores.mjs";
+</script>
+<nav>
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#profile">Profile</a></li>
+        {#if $userStore.isLoggedIn}
+        <li><a href="#home">Logout</a></li>
+        {:else}
+        <li><a href="#login">Login</a></li>
+        {/if}
+    </ul>
+</nav>
