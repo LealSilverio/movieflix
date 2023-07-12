c666d1ccd57449b02f3260ab300499c679452ac8
jonasnunn <jonasnunn@gmail.com>
2023-07-12 14:11:10 -0600

    added MyMovie card styling

--- src/js/components/MyMovieList.svelte
+++ src/js/components/MyMovieList.svelte
@@ -1,49 +1,55 @@
 <script>
-    import {userStore} from "../../lib/stores.mjs"
-    import {getUserProfile, setUserProfile} from "../../lib/supabaseClient.mjs"
-    import { searchResults } from "../../lib/stores.mjs";
-    import MovieCard from "./MovieCard.svelte";
+  import { userStore } from "../../lib/stores.mjs";
+  import { getUserProfile, setUserProfile } from "../../lib/supabaseClient.mjs";
+  import { searchResults } from "../../lib/stores.mjs";
+  import MyMovieCard from "./MyMovieCard.svelte";
+  import { checkLogin } from "../../lib/supabaseClient.mjs";
 
+  async function init() {
+    await checkLogin();
+  }
+  init();
 
-//let handleSubmit = 0;
-
-//if ($userStore.user.ID != null)
-//{
+  if ($userStore.isLoggedIn) {
     const userId = $userStore.user.ID;
     let profile = {};
-    async function init(){
-        profile = await getUserProfile(userId);
+    async function init() {
+      profile = await getUserProfile(userId);
     }
-    
+
     async function handleSubmit(e) {
-        e.preventDefault();
-        const formData = new FormData(e.target)
-        const userProfile = {};
-        userProfile.website = formData.get('website'); //corresponds to name attribute on the form
-        userProfile.full_name = formData.get('full_name');
-        setUserProfile(userProfile, userId);
-    };
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const userProfile = {};
+      userProfile.website = formData.get("website"); //corresponds to name attribute on the form
+      userProfile.full_name = formData.get("full_name");
+      setUserProfile(userProfile, userId);
+    }
     init();
 
     let userInput = "star wars";
 
     async function getData() {
-        console.log(userInput); 
-        const options = {
-        method: 'GET',
+      console.log(userInput);
+      const options = {
+        method: "GET",
         headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2Q0ODg1ODhlYTI3Nzg2M2NlOGE2MjA3ZDFiNjY3NyIsInN1YiI6IjY0OWRmN2I0MDkxZTYyMDBhZDU0OGM4YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DPwW979vHfknWm16hkBASCL59yCxcA7lYR-dBHJnYpE'
-        }
-        };
-        await fetch(`https://api.themoviedb.org/3/search/movie?query=${userInput}`, options)
-        .then(response => response.json())
-        .then(response => $searchResults = response.results)
-        .catch(err => console.error(err));
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2Q0ODg1ODhlYTI3Nzg2M2NlOGE2MjA3ZDFiNjY3NyIsInN1YiI6IjY0OWRmN2I0MDkxZTYyMDBhZDU0OGM4YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DPwW979vHfknWm16hkBASCL59yCxcA7lYR-dBHJnYpE",
+        },
+      };
+      await fetch(
+        `https://api.themoviedb.org/3/search/movie?query=${userInput}`,
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => ($searchResults = response.results))
+        .catch((err) => console.error(err));
     }
 
-    getData()
-//}
+    getData();
+  }
 </script>
 
 <h2>My Movie List</h2>
@@ -59,8 +65,19 @@
     </p>
 <button type="submit">Submit</button>
 </form> -->
-
-
-{#each $searchResults as movie}
-    <MovieCard {movie}/>
+<main>
+  {#each $searchResults as movie}
+  <MyMovieCard {movie} />
   {/each}
+</main>
+
+
+<style>
+
+main{
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+</style>
