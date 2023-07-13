6c02135bcea04f432a0a515758f7ac0852483947
jonasnunn <jonasnunn@gmail.com>
2023-07-13 10:56:15 -0600

    made mymovie card work with images

--- src/js/components/MovieCard.svelte
+++ src/js/components/MovieCard.svelte
@@ -1,17 +1,13 @@
 <script>
-  import MovieDetails from "./MovieDetails.svelte";
-
-export let movie;
-console.log(movie);
-var imageURL = `https://image.tmdb.org/t/p/original${movie.poster_path}`
+    import MovieDetails from "./MovieDetails.svelte";
+  
+  export let movie;
+  console.log(movie);
+  var imageURL = `https://image.tmdb.org/t/p/original${movie.poster_path}`
 </script>
 
-<div id="movie-card">
-    <a href="#movieDetails">
-        <img src={imageURL} alt={movie.title}>
-        <div>
-            <h3>{movie.title}</h3>
-            <p>{movie.overview}</p>
-        </div>
-    </a>
+<div>
+    <img src={movie.poster_path} alt={movie.title}>
+    <h3>{movie.title}</h3>
+    <p>{movie.overview}</p>
 </div>
--- src/js/components/MyMovieCard.svelte
+++ src/js/components/MyMovieCard.svelte
@@ -1,19 +1,35 @@
 <script>
-    export let movie;
-    console.log(movie)
+    import MovieDetails from "./MovieDetails.svelte";
+  
+  export let movie;
+  console.log(movie);
+  var imageURL = `https://image.tmdb.org/t/p/original${movie.poster_path}`
 </script>
-    
-<div>
-    <img src={movie.poster_path} alt={movie.title}>
-    <h3>{movie.title}</h3>
-    <p>{movie.overview}</p>
-</div>
+  
+  <div id="movie-card">
+      <a href="#movieDetails">
+          <img src={imageURL} alt={movie.title}>
+          <div>
+              <h3>{movie.title}</h3>
+              <p>{movie.overview}</p>
+          </div>
+      </a>
+  </div>
 
 <style>
 
     div{
         margin-right: 30px;
         margin-bottom: 30px;
+    }
+
+    img{
+        max-width: 200px;
+    }
+
+    a{
+        text-decoration: none;
+        color: black;
     }
 
 </style>
